9732a75b768692e9b39d8323b5a8e37fa30accc1
loadingim <68083530+loadingim@users.noreply.github.com>
2024-09-29 17:21:56 +0900

    Flash Light
    플래쉬 라이트 카메라 트리거 구현

--- Assets/Scripts/ItemCheck.cs
+++ Assets/Scripts/ItemCheck.cs
@@ -2,22 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
-public class PlayerController : MonoBehaviour
+public class ItemCheck : MonoBehaviour
 {
+
     [SerializeField] Transform camTransform;
-    [SerializeField] Animator animator;
-    [SerializeField] float moveSpeed, rotateSpeed;
+    [SerializeField] float rotateSpeed;
+    [SerializeField] float moveSpeed, distance;
     [SerializeField] GameObject flashLight;
-    private float xRotate, yRotate, x, z, y;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update()
+    private void Update()
     {
         Move();
         Look();
@@ -26,36 +25,23 @@ public class PlayerController : MonoBehaviour
         {
             flashLight.SetActive(flashLight.activeSelf ? false : true);
         }
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Fire();
-        }
     }
 
     private void Move()
     {
-         x = Input.GetAxis("Horizontal");
-         z = Input.GetAxis("Vertical");
-        yRotate -= Input.GetAxis("Mouse Y") * Time.deltaTime * moveSpeed;
-        xRotate += Input.GetAxis("Mouse X") * Time.deltaTime * moveSpeed;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        animator.SetFloat("MoveSpeed", moveSpeed * z);
         transform.Translate(Vector3.forward * z * moveSpeed * Time.deltaTime);
         transform.Translate(Vector3.right * x * moveSpeed * Time.deltaTime);
     }
 
     private void Look()
     {
-        x = Input.GetAxis("Mouse X"); // 마우스 좌우 움직임량
-        y = Input.GetAxis("Mouse Y"); // 마우스 위아래 움직임량
+        float x = Input.GetAxis("Mouse X"); // 마우스 좌우 움직임량
+        float y = Input.GetAxis("Mouse Y"); // 마우스 위아래 움직임량
 
         transform.Rotate(Vector3.up, rotateSpeed * x * Time.deltaTime);
         camTransform.Rotate(Vector3.right, rotateSpeed * -y * Time.deltaTime);
-    }
-
-    private void Fire()
-    {
-        
     }
 }
